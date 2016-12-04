64251dc81eb1a561618c4deb615e320da035d37c
Jamie Ramos <jmramos@live.unc.edu>
2016-12-04 01:54:06 +0000

    added audio and unzipped

--- build-presentation.sh
+++ build-presentation.sh
@@ -2,7 +2,8 @@
 
 INPUT=$1
 TEMPLATE=template-index.html
-TRANSITION=slide
-THEME=black
+TRANSITION=convex
+THEME=sky
 
 pandoc -t revealjs --template=$TEMPLATE -s --variable theme="$THEME" --variable transition="$TRANSITION" --variable revealjs-url="./" $INPUT.md -o index.html
+echo "Script has run. Presentation $1 has been made."
---
+++ present.md
@@ -0,0 +1,6 @@
+% THIS IS A PRESENTATION
+% gavvy and jamiemramos
+% Decemeber 5, 2016
+
+<aside class="notes"> THIS IS A NOTE! </aside>
+THIS IS TEXT!
